3a5e96825be1bb84094f649689456f98ffe35c62
DrueWolf <dwolf0305@gmail.com>
2024-10-09 20:42:24 -0400

    signUp -> signup

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const router = express.Router()
 const userController = require('../controllers/users.js')
 
-router.post('/signUp', userController.createUser)
+router.post('/signup', userController.createUser)
 router.post('/login', userController.loginUser)
 router.put('/:id', userController.auth, userController.updateUser)
 router.delete('/:id', userController.auth, userController.deleteUser)
